6386dc4384a9337541a04e3da97fea793d061175
Kazuki Yoshikawa <yoshikawa.kazuki14@gmail.com>
2020-05-09 12:06:30 +0900

    PDF create

--- src/daitan-api/app/Http/Controllers/PDFController.php
+++ src/daitan-api/app/Http/Controllers/PDFController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use PDF;
+
 class PDFController extends Controller
 {
-    //
+    public function index(){
+
+        $pdf = PDF::loadView('pdf/pdf_1');
+        return $pdf->download('hello.pdf');
+    }
 }
